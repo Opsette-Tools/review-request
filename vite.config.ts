d9aa99f8c1504cca0fa1f94a42264bcfdf131558
deebuilt <ruthnie@deebuilt.co>
2026-06-15 13:15:43 -0400

    Bump deploy actions to Node 24 versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
@@ -6,7 +6,7 @@ export default defineConfig({
   base: "/review-request/",
   server: {
     host: "::",
-    port: 8080,
+    port: 8111,
   },
   plugins: [react()],
   resolve: {
